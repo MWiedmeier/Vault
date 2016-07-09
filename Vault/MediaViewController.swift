8d6aef16a64aea9a522be0b0650a3d61af52bf80
MWiedmeier <mattwiedmeier@gmail.com>
2016-07-09 17:56:22 -0400

    Fixed resolution of images added to UICollectionView.

---
+++ Vault/MediaViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MediaViewController.swift
+//  Vault
+//
+//  Created by Matthew Wiedmeier on 7/7/16.
+//  Copyright © 2016 Matthew Wiedmeier. All rights reserved.
+//
+
+import UIKit
+
+class MediaViewController: UICollectionViewController {
+    
+    private let reuseIdentifier = "MediaCell"
+    private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom:50.0, right:20.0)
+    
+
+}
--- Vault/VaultViewController.swift
+++ Vault/VaultViewController.swift
@@ -8,17 +8,34 @@
 
 import UIKit
 
-class VaultViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UICollectionViewDataSource, UICollectionViewDelegate {
+class VaultViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     @IBOutlet weak var addButton: UIBarButtonItem!
     @IBOutlet weak var menuButton: UIBarButtonItem!
     @IBOutlet weak var collectionView: UICollectionView!
     
-    let reuseIdentifier = "cell"
+    let reuseIdentifier = "MediaCell"
+    let sectionInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    let cellSpacing = 5.0
+    
+    var screenSize: CGRect!
+    var screenWidth: CGFloat!
+    var screenHeight: CGFloat!
+    
     var thumbnails: [UIImage] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        screenSize = UIScreen.mainScreen().bounds
+        screenWidth = screenSize.width
+        screenHeight = screenSize.height
+        
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 0, bottom: 10, right: 0)
+        layout.itemSize = CGSize(width: screenWidth / 4, height: screenWidth / 4)
+        layout.minimumInteritemSpacing = 0
+        layout.minimumLineSpacing = 0
         
         // Do any additional setup after loading the view.
         if self.revealViewController() != nil {
@@ -43,16 +60,21 @@ class VaultViewController: UIViewController, UIImagePickerControllerDelegate, UI
         }
     }
     
+    
     // MARK: - UIImagePickerControllerDelegate Methods
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        dismissViewControllerAnimated(true, completion: nil)
+
         if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            dismissViewControllerAnimated(true, completion: nil)
+
             thumbnails.append(pickedImage)
             NSOperationQueue.mainQueue().addOperationWithBlock(collectionView.reloadData)
 
             
         }
-        dismissViewControllerAnimated(true, completion: nil)
     }
     
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
@@ -63,7 +85,6 @@ class VaultViewController: UIViewController, UIImagePickerControllerDelegate, UI
     
     // tell the collection view how many cells to make
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        print(self.thumbnails.count)
         return self.thumbnails.count
     }
     
@@ -74,6 +95,8 @@ class VaultViewController: UIViewController, UIImagePickerControllerDelegate, UI
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! ThumbnailViewController
         
         cell.imageThumbnail.image = self.thumbnails[indexPath.item]
+        cell.imageThumbnail.contentMode = UIViewContentMode.ScaleAspectFill
+        
         
         return cell
     }
@@ -85,5 +108,47 @@ class VaultViewController: UIViewController, UIImagePickerControllerDelegate, UI
         // handle tap events
         print("You selected cell #\(indexPath.item)!")
     }
+    
+    //
+    func collectionView(collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                               sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize{
+        
+        return CGSize(width: screenWidth/4, height: screenWidth/4);
+        
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAtIndex section: Int) -> CGFloat {
+        return 0;
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat {
+        return 1;
+    }
+//        
+//    func collectionView(collectionView: UICollectionView,
+//                        layout collectionViewLayout: UICollectionViewLayout,
+//                               insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+//        
+//        return sectionInsets
+//    }
+//    
+//    func scaleToFitSize(image:UIImage, size:CGSize) -> UIImage{
+//        
+//        let scale = min(size.width/image.size.width, size.height/image.size.height)
+//        let width = image.size.width * scale
+//        let height = image.size.height * scale
+//        
+//        let imageRect = CGRectMake((size.width-width)/2.0,
+//                                   (size.height-height)/2.0,
+//                                   width,
+//                                   height)
+//        
+//        UIGraphicsBeginImageContextWithOptions(size, false, 0)
+//        image.drawInRect(imageRect)
+//        return UIGraphicsGetImageFromCurrentImageContext()
+//    }
+    
 }
+
 
